4e5da261805f58ef50dcd4bd39fcaddcfdc9701f
ankittiwari <ankit90499@gmail.com>
2025-08-21 11:05:35 +0530

    updated UI

--- App.tsx
+++ App.tsx
@@ -11,26 +11,24 @@ import { Platform, StyleSheet, Image, View, Text } from 'react-native';
 function App() {
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textpoc}>POC with rn 80, react 19, rnw 0.21.0 </Text>
-      <Image style={styles.image} source={require('./images/dependencies.png')} />
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.textpoc}>POC with rn 80, react 19, rnw 0.21.0 </Text>
+        <Image style={styles.image} source={require('./images/dependencies.png')} />
 
 
-      {Platform.OS === "web" ? (
-        <a href="https://github.com/ankitt8/cross-platform-myapp" target="_blank" rel="noopener noreferrer">
-          Code link
-        </a>
-      ) : null}
-      {Platform.OS === "web" ? (
-        <a href="https://github.com/facebook/react-native/blob/0ef21bf8ad9836ad34f459795e180dd3e34f7618/package.json#L106" target="_blank" rel="noopener noreferrer">
-          rn github code same is mentioned as dev dependency in their codebase for rn 80 & greater version
-        </a>
-      ) : null}
-
-
-
-
-      <View style={{ marginTop: 20, width: '100%' }}>
+        {Platform.OS === "web" ? (
+          <a href="https://github.com/ankitt8/cross-platform-myapp" target="_blank" rel="noopener noreferrer">
+            Code link
+          </a>
+        ) : null}
+        {Platform.OS === "web" ? (
+          <a href="https://github.com/facebook/react-native/blob/0ef21bf8ad9836ad34f459795e180dd3e34f7618/package.json#L106" target="_blank" rel="noopener noreferrer">
+            rn github code same is mentioned as dev dependency in their codebase for rn 80 & greater version
+          </a>
+        ) : null}
+      </View>
+      <View style={{ marginTop: 20, width: '100%', ...styles.container }}>
         <Text style={styles.textpoc}>POC with rn 80, react 18, rnw 0.21.0 </Text>
         <Image style={styles.image} source={require('./images/dependencies.png')} />
         <Text style={styles.text}>rn and rnw starts complaining of peer react version 19 needed </Text>
